51e0eafa657b68b7a4dce3e5dd084d1fa0f84787
burcakA <burcakgridag@gmail.com>
2017-06-16 16:39:22 +0200

    accelerometer and Gyro for makeblock

--- blocks/makeblockSensors.js
+++ blocks/makeblockSensors.js
@@ -81,41 +81,40 @@ Blockly.Blocks['makeblockSensors_gyroscope_getSample'] = {
      * @constructs makeSensors_gyroscope_getSample
      * @this.Blockly.Block
      * @param {String/dropdown}
-     *            MODE - angle or acceleration mode of the gyroscope
-     * @param {String/dropdown}
-     *            GYROAXIS - X, Y or Z axis of the gyroscope.
-     * @param {String/dropdown}
-     *            SENSORPORT - 1, 2, 3 or 4
+     *            GYROAXIS - X, Y  axis of the gyroscope.
      * @returns immediately
      * @returns {Number}
      * @memberof Block
      */
-
     init : function() {
         this.setColour(Blockly.CAT_SENSOR_RGB);
-        var axis = new Blockly.FieldDropdown([ [ 'X', 'X' ], [ 'Y', 'Y' ], ['Z', 'Z'] ],
-            function(option) {
-               this.updateShape_(option);
-        });
-        var sensorPort = new Blockly.FieldDropdown([ [ 'Port 1', '1' ], [ 'Port 2', '2' ], [ 'Port 3', '3' ], [ 'Port 4', '4' ] ]);
-        var mode = new Blockly.FieldDropdown([ [ 'angle', 'angle' ], [ 'acceleration', 'acceleration' ] ]);
-        this.appendDummyInput().appendField(mode, 'GYROMODE').appendField(Blockly.Msg.SENSOR_GET_SAMPLE).appendField(axis, 'GYROAXIS').appendField(Blockly.Msg.SENSOR_GYRO).appendField(sensorPort, 'SENSORPORT');
+        var dropdown = new Blockly.FieldDropdown([['X', 'X'], ['Y', 'Y']]);
+        var sensorPort = new Blockly.FieldDropdown([['Port 1', '1'], ['Port 2', '2'], ['Port 3', '3'], ['Port 4', '4']]);
+        this.appendDummyInput().appendField(Blockly.Msg.SENSOR_GET_SAMPLE).appendField(dropdown, 'MODE').appendField(Blockly.Msg.SENSOR_GYRO).appendField(sensorPort, 'SENSORPORT');
         this.setOutput(true, 'Number');
         this.setTooltip(Blockly.Msg.GYRO_TOOLTIP);
-    },
+    }
+};
 
-    updateShape_ : function(option) {
-        var input = this.getInput('DROPDOWN');
-        for (var i = 0, field; field = input.fieldRow[i]; i++) {
-            console.log(field.name);
-            if (field.name === 'SENSORTYPE' || field.name === 'GET') {
-                continue;
-            }
-            toRemove.push(field.name);
-        }/*
-        for (var j = 0; j < toRemove.length; j++) {
-            input.removeField(toRemove[j]);
-        }*/
+Blockly.Blocks['makeblockSensors_accelerometer_getSample'] = {
+    /**
+     * Get a sample from the gyroscope.
+     *
+     * @constructs makeSensors_accelerometer_getSample
+     * @this.Blockly.Block
+     * @param {String/dropdown}
+     *            GYROAXIS - X, Y,Z  axis of the acceleremoter.
+     * @returns immediately
+     * @returns {Number}
+     * @memberof Block
+     */
+    init : function() {
+        this.setColour(Blockly.CAT_SENSOR_RGB);
+        var dropdown = new Blockly.FieldDropdown([['X', 'X'], ['Y', 'Y'], ['Z', 'Z']]);
+        var sensorPort = new Blockly.FieldDropdown([['Port 1', '1'], ['Port 2', '2'], ['Port 3', '3'], ['Port 4', '4']]);
+        this.appendDummyInput().appendField(Blockly.Msg.SENSOR_GET_SAMPLE).appendField(dropdown, 'COORDINATE').appendField(Blockly.Msg.NAO_ACCELEROMETER).appendField(sensorPort, 'SENSORPORT');
+        this.setOutput(true, 'Number');
+        this.setTooltip(Blockly.Msg.GYRO_TOOLTIP);
     }
 };
 
