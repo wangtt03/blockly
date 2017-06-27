6d1757faeb7266b4defcf39b83b7afcc8581df7c
burcakA <burcakgridag@gmail.com>
2017-06-27 12:03:35 +0200

    makeblock flamesensor added

--- blocks/makeblockSensors.js
+++ blocks/makeblockSensors.js
@@ -53,6 +53,27 @@ Blockly.Blocks['makeblockSensors_accelerometer_getSample'] = {
     }
 };
 
+Blockly.Blocks['makeblockSensors_flameSensor_getSample'] = {
+    /**
+     * Get a sample from the flame sensor.
+     *
+     * @constructs makeSensors_flameSensor_getSample
+     * @this.Blockly.Block
+     * @param {String/dropdown}
+     *           SENSORPORT- 1-4
+     * @returns immediately
+     * @returns {Number}
+     * @memberof Block
+     */
+    init : function() {
+        this.setColour(Blockly.CAT_SENSOR_RGB);
+        var sensorPort = new Blockly.FieldDropdown([['Port 1', '1'], ['Port 2', '2'], ['Port 3', '3'], ['Port 4', '4']]);
+        this.appendDummyInput().appendField(Blockly.Msg.SENSOR_GET_SAMPLE).appendField(Blockly.Msg.SENSOR_FLAME).appendField(sensorPort, 'SENSORPORT');
+        this.setOutput(true, 'Number');
+        this.setTooltip(Blockly.Msg.FLAME_TOOLTIP);
+    }
+};
+
  Blockly.Blocks['makeblockSensors_ambientlight'] = {
      /**
       *
